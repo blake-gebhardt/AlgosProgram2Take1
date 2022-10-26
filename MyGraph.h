fcd8495fc4242f348d798109ed58238921782f8b
blake-gebhardt <blakewgebhardt@gmail.com>
2022-10-26 09:17:01 -0500

    Mygraph done as a vector of edge structs.

--- MyGraph.h
+++ MyGraph.h
@@ -5,26 +5,54 @@
 #ifndef UALGOSPROGRAM2TAKE1_MYGRAPH_H
 #define UALGOSPROGRAM2TAKE1_MYGRAPH_H
 #include "vector"
+#include "iostream"
+#include "vector"
 
 class MyGraph{
 public:
 
-    int** graphMatrix;
+    struct edge{
+        int point1;
+        int point2;
+        float weight;
+    };
+
+    //array of <start, end, weight> values
+    std::vector<edge> edgeList;
     int graphSize;
 
     MyGraph(int n) {
         graphSize = n;
-        graphMatrix = new int*[n];
-        for (int i = 0; i < n; i++) {
-            graphMatrix[i] = new int[n];
-        }
     };
 
     MyGraph(const MyGraph& g){
-        this->graphMatrix = g.graphMatrix;
+        for (int i = 0; i < this->edgeList.size(); i++){
+            this->edgeList[i] = g.edgeList[i];
+        }
     };
 
+    bool inline AddEdge(int a, int b, float w){
+        if ((a > graphSize) || (b > graphSize) ){
+            return false;
+        }
+        else {
+            edge toAdd = {a, b, w};
+            edgeList.push_back(toAdd);
+            return true;
+        }
+    }
 
+    void inline Output(std::ostream &os) {
+        std::cout << "Vertices: " << graphSize << "\n";
+        for (auto x : edgeList){
+            if (x.point1 < x.point2){
+            os << x.point1 << " " << x.point2 << " " << x.weight << "\n";
+            }
+            else {
+                os << x.point2 << " " << x.point1 << " " << x.weight << "\n";
+            }
+        }
+    }
 
 
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <chrono>
+#include "MyGraph.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    MyGraph graph(5);
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100000; i ++){
+    graph.AddEdge(0,1, 3);
+    graph.AddEdge(0,4, 3);
+    graph.AddEdge(1,4, 3);
+    graph.AddEdge(1,3, 3);
+    graph.AddEdge(1,2, 3);
+    graph.AddEdge(2,3, 3);
+    graph.AddEdge(4,3, 3);
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    graph.Output(std::cout);
+    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Time: " << duration.count();
     return 0;
 }
